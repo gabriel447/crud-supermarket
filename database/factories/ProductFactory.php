9ca5448c602e34a659c32b12fcce6165b9ac6334
Gabriel447 <fullstackgabe.com>
2024-01-06 16:03:19 -0300

    create factories and seeds

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word,
+            'description' => $this->faker->text,
+            'price' => $this->faker->randomFloat(2, 0, 1000), 
+            'stock' => $this->faker->numberBetween(0, 100), 
+        ];
+    }
+}
