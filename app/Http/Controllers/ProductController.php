0c8a9039b56c6e59b3ba853a69001a88bd5cec75
Gabriel447 <fullstackgabe.com>
2024-01-06 16:32:02 -0300

    api config and update controller and tests methods: get, post, put, delete (its ok)

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -9,12 +11,7 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::all();
-        return view('products.index', compact('products'));
-    }
-
-    public function create()
-    {
-        return view('products.create');
+        return response()->json($products);
     }
 
     public function store(Request $request)
@@ -26,15 +23,14 @@ class ProductController extends Controller
             'stock' => 'required|integer'
         ]);
 
-        Product::create($validatedData);
-        return redirect()->route('products.index');
+        $product = Product::create($validatedData);
+        return response()->json($product, 201);
     }
 
-    public function show($id)
-    {
-        $product = Product::findOrFail($id);
-        return view('products.show', compact('product'));
-    }
+    public function show(Product $product)
+       {
+           return response()->json($product);
+       }
 
     public function edit($id)
     {
@@ -42,22 +38,22 @@ class ProductController extends Controller
         return view('products.edit', compact('product'));
     }
 
-    public function update(Request $request, $id)
-    {
-        $validatedData = $request->validate([
-            'name' => 'required|max:255',
-            'description' => 'nullable',
-            'price' => 'required|numeric',
-            'stock' => 'required|integer'
-        ]);
-        
-        Product::whereId($id)->update($validatedData);
-        return redirect()->route('products.index');
-    }
+    public function update(Request $request, Product $product)
+      {
+          $validatedData = $request->validate([
+              'name' => 'sometimes|required|max:255',
+              'description' => 'nullable',
+              'price' => 'sometimes|required|numeric',
+              'stock' => 'sometimes|required|integer'
+          ]);
 
-    public function destroy($id)
-    {
-        Product::findOrFail($id)->delete();
-        return redirect()->route('products.index');
-    }
+          $product->update($validatedData);
+          return response()->json($product);
+      }
+
+      public function destroy(Product $product)
+      {
+          $product->delete();
+          return response()->json(null, 204);
+      }
 }
