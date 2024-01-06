a41dc808a113cc8f559c082725b2edb9405bf52d
Gabriel447 <fullstackgabe.com>
2024-01-06 13:24:10 -0300

    create Controller and Route

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return view('products.index', compact('products'));
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'nullable',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer'
+        ]);
+
+        Product::create($validatedData);
+        return redirect()->route('products.index');
+    }
+
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+        return view('products.show', compact('product'));
+    }
+
+    public function edit($id)
+    {
+        $product = Product::findOrFail($id);
+        return view('products.edit', compact('product'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'nullable',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer'
+        ]);
+        
+        Product::whereId($id)->update($validatedData);
+        return redirect()->route('products.index');
+    }
+
+    public function destroy($id)
+    {
+        Product::findOrFail($id)->delete();
+        return redirect()->route('products.index');
+    }
+}
